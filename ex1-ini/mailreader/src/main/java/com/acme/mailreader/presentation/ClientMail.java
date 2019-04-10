eeb3ce84e7a62a614e9ae2cf1a7ec6f52f36c2c5
hugofch <hugo.fichot@outlook.fr>
2019-04-10 21:45:35 +0200

    correction dossiers

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/presentation/ClientMail.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/presentation/ClientMail.java
@@ -1,5 +1,12 @@
-package com.acme.mailreader.presentation;
+package com.acme.mailreader.domaine;
 
 public class ClientMail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Production = true, recette = false
+		boolean production = false;
+	}
 
 }
